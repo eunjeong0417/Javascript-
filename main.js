ae364188bbdd201ec2265ef8ce107315fd8f3134
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-06-30 13:55:26 +0900

    javascript basic function

--- main.js
+++ main.js
@@ -1,11 +1,63 @@
-const text = document.querySelector('.content h2');
-const popup = document.querySelector('.popup');
+//document.getElementById('count-el').innerText = 15;
 
-text.addEventListener('click', ()=>{
-    popup.classList.add('event');
-    copyClipboard();
+// let count = 0;
+
+// console.log(count)
+// console.log(age)
+
+// let myAge = 21;
+// let age = myAge;
+
+// let myAge = 21;
+// let dogAge = 8;
+// let myDogAge = myAge * dogAge ;
+// console.log(myDogAge);
+
+// let count = 50
+// count = count + 50
+
+// count = count - 75
+// count = count + 45
+// console.log(count)
+
+
+
+// function call () {
+//     console.log(42)
+// }
+
+// call()
+
+// let lap1 = 34
+// let lap2 = 33
+// let lap3 = 36
+
+// function sum () {
+//     console.log( lap1 + lap2 + lap3 )
+// }
+
+// sum()
+
+// let lapsCompleted = 0
+
+// function countSum () {
+//     lapsCompleted = lapsCompleted + 1
+// }
+
+// countSum ()
+// countSum ()
+// countSum ()
+// console.log(lapsCompleted)
+
+
+//counter app 만들기
+
+const countText = document.getElementById('count-el')
+const countBtn = document.getElementById('increment-btn')
+
+let count = 0
+
+
+countBtn.addEventListener('click', () => {
+    countText.innerText = count ++
 })
-
-const copyClipboard = () => {
-    
-}
