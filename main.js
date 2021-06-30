8384b6c990bf2b1d07c483e98cbea4acf6f0905c
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-06-30 18:04:05 +0900

    counter app 만들기

--- main.js
+++ main.js
@@ -1,63 +1,57 @@
-//document.getElementById('count-el').innerText = 15;
-
-// let count = 0;
-
-// console.log(count)
-// console.log(age)
-
-// let myAge = 21;
-// let age = myAge;
-
-// let myAge = 21;
-// let dogAge = 8;
-// let myDogAge = myAge * dogAge ;
-// console.log(myDogAge);
-
-// let count = 50
-// count = count + 50
-
-// count = count - 75
-// count = count + 45
-// console.log(count)
-
-
-
-// function call () {
-//     console.log(42)
-// }
-
-// call()
-
-// let lap1 = 34
-// let lap2 = 33
-// let lap3 = 36
-
-// function sum () {
-//     console.log( lap1 + lap2 + lap3 )
-// }
-
-// sum()
-
-// let lapsCompleted = 0
-
-// function countSum () {
-//     lapsCompleted = lapsCompleted + 1
-// }
-
-// countSum ()
-// countSum ()
-// countSum ()
-// console.log(lapsCompleted)
-
 
 //counter app 만들기
 
-const countText = document.getElementById('count-el')
-const countBtn = document.getElementById('increment-btn')
+let countText = document.getElementById('count-el')
+let countBtn = document.getElementById('increment-btn')
+let saveEl = document.getElementById('save-el')
 
 let count = 0
 
-
 countBtn.addEventListener('click', () => {
-    countText.innerText = count ++
+    //count = count + 1
+    count += 1
+    countText.textContent = count 
 })
+
+//save 기능 만들기
+
+
+function save () {
+    let countStr = " " + count +" - "
+    // saveEl.innerText += countStr
+     saveEl.textContent += countStr
+
+     //기존에 있는 text에 countStr을 더한다
+     countText.textContent = 0
+     count = 0
+}
+
+function reset () {
+    count = 0 
+    countText.textContent = count
+}
+
+
+
+
+
+
+// let a ="per"
+
+// let message = "You have tree new notifications"
+
+// console.log( message + ", " + a )
+
+// let naming = "eunjeong"
+// let greeting = "Hi, my name is "
+
+// let mygreeting = greeting +""+naming
+// console.log(mygreeting)
+
+// let welcomeEl = document.getElementById("welcome-el")
+
+// let naming = "eunjeong"
+// let greeting = "welcome"
+
+
+// welcomeEl.innerText = greeting +" "+ naming + "😊"
