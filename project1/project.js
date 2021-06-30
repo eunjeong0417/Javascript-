8c0074cc0ac0ef7cae4780af505132780f9dfadd
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-06-30 23:52:28 +0900

    button에 forEach문 적용하기

--- project1/project.js
+++ project1/project.js
@@ -1,21 +1,43 @@
-let btnDecrease = document.querySelector('.decrease')
-let btnReset = document.querySelector('.reset')
-let btnIncrease = document.querySelector('.increase')
-let span = document.getElementById('value')
+//set initial count
 let count = 0
 
-btnDecrease.addEventListener('click', () => {
-    count = count - 1
-    span.textContent = count
+let span = document.getElementById('value')
+//id가 value인 span 태그 가져오기
+//let span = document.querySelector('#value)와 동일함
 
+let btns = document.querySelectorAll('.btn');
+
+btns.forEach(function(btn) {
+    btn.addEventListener('click', function(e){
+        const btnClass = e.currentTarget.classList;
+        if(btnClass.contains("decrease")) {
+            count = count - 1
+            span.textContent = count
+        }
+        else if (btnClass.contains("reset")) {
+            count = 0
+            span.textContent = count
+        } else {
+            count = count + 1
+            span.textContent = count
+        }
+    })
 })
 
-btnReset.addEventListener('click', () => {
-    count = 0
-    span.textContent = count
-})
 
-btnIncrease.addEventListener('click', () => {
-    count = count + 1
-    span.textContent = count
-})
+
+// btnDecrease.addEventListener('click', () => {
+//     count = count - 1
+//     span.textContent = count
+
+// })
+
+// btnReset.addEventListener('click', () => {
+//     count = 0
+//     span.textContent = count
+// })
+
+// btnIncrease.addEventListener('click', () => {
+//     count = count + 1
+//     span.textContent = count
+// })
