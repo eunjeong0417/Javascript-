fdfde4870fbcd8b33d12338820cca31ff739c591
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-07-01 16:36:48 +0900

    변수에 함수를 할당해서 사용하기

--- Blackjack/index.js
+++ Blackjack/index.js
@@ -3,8 +3,8 @@ let messageEl = document.getElementById('message-el')
 let sumEl = document.getElementById('sum-el')
 let cardsEl = document.getElementById('cards-el')
 //뽑는 모든 카드를 가져오기 위해 배열을 만든다
-let firstCard = 10
-let secondCard = 7
+let firstCard = getRandomCard ()
+let secondCard = getRandomCard ()
 let cards = [firstCard, secondCard] 
 //Arrays - ordered lists of items
 let sum = firstCard + secondCard 
@@ -12,12 +12,25 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 
+function getRandomCard () {
+    return 5
+}
+
+
+function newCard () {
+    let card = getRandomCard ()
+    sum += card
+    cards.push(card)
+    renderGame()
+}
 
 btnClicked.addEventListener('click', startGame)
 
 function startGame () {
     renderGame()
 }
+
+
 
 function renderGame () {
 
@@ -31,20 +44,18 @@ function renderGame () {
             isAlive = false
             message = "we're logging out"
         }
-        cardsEl.textContent = "Cards:" + cards
+        cardsEl.textContent = "Cards:"
 //firstCard와 secondCard를 cards 배열에 있는 항목으로 변경한다
+        for( let i = 0; i < cards.length; i++) {
+            cardsEl.textContent += " " + cards[i] + " "
+        }
+
         messageEl.textContent = message
         sumEl.textContent = "Sum:"+" "+ sum
 }
-//+=을 쓰면 기존 텍스트에 덧붙여진다 따라서 새로운
-//text를 반환하도록 만드려면 = 만 사용한다
+// +=을 쓰면 기존 텍스트에 덧붙여진다 따라서 새로운
+// text를 반환하도록 만드려면 = 만 사용한다
 
-function newCard () {
-    let card = 5
-    sum += card
-    cards.push(card)
-    renderGame ()
-}
 
 //Array 만들기
 
@@ -76,3 +87,42 @@ function newCard () {
 // //pop을 사용하면 배열의 마지막 항목 삭제
 // messages.pop()
 // console.log(messages)
+
+//for
+
+// for (let count = 10; count < 21; count += 1) {
+//     console.log(count)
+// }
+
+// for (let i = 10; i < 101; i += 10 ) {
+//     console.log(i)
+// }
+
+// let cards = [7, 3, 4]
+
+// for (i = 0;  i < cards.length; i++ ) {
+//     console.log(cards[i])
+// }
+
+// let sentence = ["hello", "my", "name", "is", "eunjeong"]
+// let greetingEl = document.getElementById('greeting-el')
+
+// for ( let i = 0;  i < sentence.length; i ++) {
+//     greetingEl.textContent +=" " + sentence[i] + " "
+// }
+
+// let player1Time = 102
+// let player2Time = 107
+
+// function totalRace () {
+//     if ( player1Time < player2Time) {
+//         return player2Time
+//     } else if ( player1Time > player2Time) {
+//         return player1Time
+//     } else {
+//         return player1Time
+//     }
+// } 
+
+// let newTotalValue = totalRace()
+// console.log(newTotalValue)
