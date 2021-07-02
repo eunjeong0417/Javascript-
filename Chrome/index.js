cd4dc1ffa734b1ca44a5a0c57127bc10a2e2a956
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-07-02 18:33:39 +0900

    innerHTML로 li태그 만들기

--- Chrome/index.js
+++ Chrome/index.js
@@ -1,11 +1,33 @@
 let inputBtn = document.getElementById('input-btn')
-let boxEl = document.getElementById('box')
+let myLeads = []
+const inputEl = document.getElementById('input-el')
+const ulEl = document.getElementById('ul-el')
+
+
+
 
 inputBtn.addEventListener('click', () => {
-    console.log("Button clicked!")
+    myLeads.push(inputEl.value)
+    renderLeads()
+//save input 버튼을 클릭하면 input의 value를
+//myLeads 배열에 push하고
+//renderLeads함수 호출
 })
 
-boxEl.addEventListener('click', () => {
-    console.log("I want to open the box")
-    document.body.style.backgroundColor = "darkblue";
-})
+function renderLeads () {
+    let listItems = ""
+    for( let i = 0; i < myLeads.length; i++) {
+        listItems += "<li>" + myLeads[i] + "</li>"
+        ulEl.innerHTML = listItems
+        console.log(ulEl)
+    }
+}
+
+
+
+// containerEl.innerHTML = "<button onclick='buy()'>buy!</button>"
+
+// function buy () {
+//     containerEl.innerHTML += "<p>Thank you for buying</p>"
+//     console.log(containerEl)
+// }
