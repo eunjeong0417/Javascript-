999c60e97ad4c0bf4802ef08e9532e88fa486084
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-07-02 20:08:28 +0900

    Template literals

--- Chrome/index.js
+++ Chrome/index.js
@@ -2,25 +2,38 @@ let inputBtn = document.getElementById('input-btn')
 let myLeads = []
 const inputEl = document.getElementById('input-el')
 const ulEl = document.getElementById('ul-el')
+let deleteEl = document.getElementById('delete-btn')
+let listItems = ""
 
 
-
+deleteEl.addEventListener('click', () => {
+    ulEl.innerHTML = ""
+})
 
 inputBtn.addEventListener('click', () => {
     myLeads.push(inputEl.value)
-    renderLeads()
+    inputEl.value = ""
 //save input 버튼을 클릭하면 input의 value를
 //myLeads 배열에 push하고
 //renderLeads함수 호출
+renderLeads()
 })
 
+//template literal
 function renderLeads () {
     let listItems = ""
     for( let i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + "</li>"
-        ulEl.innerHTML = listItems
-        console.log(ulEl)
+        listItems += `
+         <li>
+                <a target='_blank'
+                    href='${myLeads[i]}'>
+                ${myLeads[i]}
+            </a>
+         </li>
+         `
+        
     }
+    ulEl.innerHTML = listItems
 }
 
 
@@ -31,3 +44,16 @@ function renderLeads () {
 //     containerEl.innerHTML += "<p>Thank you for buying</p>"
 //     console.log(containerEl)
 // }
+
+//template strings/literals
+
+// const recipient = "James"
+// const sender = "eunjeong"
+
+// const email = `Hey ${recipient}! 
+
+// How is it going? 
+
+// Cheers ${sender}`
+
+// console.log(email)
